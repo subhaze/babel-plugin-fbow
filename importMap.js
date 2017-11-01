d8f322d2fc7f5d65d9b408a4a5771fb1058c06af
Michael Russell <mrussell@tombras.com>
2017-11-01 10:22:41 -0400

    Cleaning up and adding test.
    - Added .eslintrc
    - Added jest tests
    - Restructured importMaps

--- importMap.js
+++ importMap.js
@@ -1,11 +1,15 @@
 module.exports = {
-	"closest": "fbow/Element/closest",
-	"matches": "fbow/Element/matches",
-	"after":"fbow/ChildNode/after",
-	"before": "fbow/ChildNode/before",
-	"remove": "fbow/ChildNode/remove",
-	"replaceWith": "fbow/ChildNode/replaceWith",
-	"append": "fbow/ParentNode/append",
-	"prepend": "fbow/ParentNode/prepend",
-	"CustomEvent": "fbow/Event/CustomEvent"
+	MemberExpression: {
+		closest: "fbow/Element/closest",
+		matches: "fbow/Element/matches",
+		after: "fbow/ChildNode/after",
+		before: "fbow/ChildNode/before",
+		remove: "fbow/ChildNode/remove",
+		replaceWith: "fbow/ChildNode/replaceWith",
+		append: "fbow/ParentNode/append",
+		prepend: "fbow/ParentNode/prepend",
+	},
+	NewExpression: {
+		CustomEvent: "fbow/Event/CustomEvent",
+	},
 };
--- index.js
+++ index.js
@@ -1,45 +1,42 @@
 const importMap = require("./importMap");
-const importMapKeys = Object.keys(importMap);
 
-module.exports = function ({ types: t }) {
-  const addedFills = [];
-  function addImport(path, polyfillPath) {
-    if (addedFills.includes(polyfillPath)) return;
-    addedFills.push(polyfillPath);
-    const importDec = t.importDeclaration(
-      [],
-      t.stringLiteral(polyfillPath),
-    );
+const MemberExpressionKeys = Object.keys(importMap.MemberExpression);
+const NewExpressionKeys = Object.keys(importMap.NewExpression);
 
-    importDec._blockHoist = 3;
-    const programPath = path.find(path => path.isProgram());
-    programPath.unshiftContainer("body", importDec);
-  }
+module.exports = function main({ types: t }) {
+	const addedFills = [];
+	function addImport(path, polyfillPath) {
+		if (addedFills.includes(polyfillPath)) return;
+		addedFills.push(polyfillPath);
+		const importDec = t.importDeclaration([], t.stringLiteral(polyfillPath));
 
-  return {
-    name: "for-better-or-worse",
-    visitor: {
-      MemberExpression: function (path, state) {
-        if (state.file.opts.sourceType === "script") return;
-        if (t.isIdentifier(path.node.property)) {
-          if(importMapKeys.includes(path.node.property.name)) {
-            addImport(path, importMap[path.node.property.name]);
-          }
-        }
-      },
-      NewExpression: function(path, state) {
-        if (state.file.opts.sourceType === "script") return;
-        if(t.isIdentifier(path.node.callee)){
-          switch (path.node.callee.name) {
-            case "CustomEvent":
-              addImport(path, importMap.CustomEvent)
-              break;
-          
-            default:
-              break;
-          }
-        }
-      }
-    }
-  };
+		importDec._blockHoist = 3; // eslint-disable-line
+		const programPath = path.find(p => p.isProgram());
+		programPath.unshiftContainer("body", importDec);
+	}
+
+	return {
+		name: "for-better-or-worse",
+		visitor: {
+			MemberExpression(path, state) {
+				if (state.file.opts.sourceType === "script") return;
+				if (t.isIdentifier(path.node.property)) {
+					if (MemberExpressionKeys.includes(path.node.property.name)) {
+						addImport(
+							path,
+							importMap.MemberExpression[path.node.property.name],
+						);
+					}
+				}
+			},
+			NewExpression(path, state) {
+				if (state.file.opts.sourceType === "script") return;
+				if (t.isIdentifier(path.node.callee)) {
+					if (NewExpressionKeys.includes(path.node.callee.name)) {
+						addImport(path, importMap.NewExpression.CustomEvent);
+					}
+				}
+			},
+		},
+	};
 };
